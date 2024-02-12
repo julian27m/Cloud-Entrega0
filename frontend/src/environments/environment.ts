630da48a0d97d47032fbd8246cd3691e84158340
Julián Mora <j.morav@uniandes.edu.co>
2024-02-11 22:43:35 -0500

    Final

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://3.92.66.67:8080'
-    //apiUrl: 'http://localhost:5000'
+    //apiUrl: 'http://3.92.66.67:8080'
+    apiUrl: 'http://localhost:5000'
   };
